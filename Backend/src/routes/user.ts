35d566dda9c40ddfe53293bfdd2c76f5ad6415ef
Krishna <leelakrishna682@gmail.com>
2024-04-29 19:57:54 +0530

    Update user.ts

--- Backend/src/routes/user.ts
+++ Backend/src/routes/user.ts
@@ -5,6 +5,7 @@ import { withAccelerate } from '@prisma/extension-accelerate';
 export const userRouter = new Hono();
 
 userRouter.post('/signup', async (req, res) => {
+    console.log('signup')
 
     
 });
