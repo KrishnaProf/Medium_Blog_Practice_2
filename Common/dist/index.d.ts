5f356d1dac25885bf117b059fb96ba7377d4fcbc
Leela_Krishna <leela_krishna@dell.com>
2024-05-03 11:33:06 +0530

    Front end code updated

---
+++ Common/dist/index.d.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+export declare const signupInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+    name: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+    name: string;
+}, {
+    email: string;
+    password: string;
+    name: string;
+}>;
+export declare type SignupType = z.infer<typeof signupInput>;
+export declare const signinInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export declare type SigninType = z.infer<typeof signinInput>;
+export declare const createPostInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+}, {
+    title: string;
+    content: string;
+}>;
+export declare type CreatePostType = z.infer<typeof createPostInput>;
+export declare const updatePostInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+}, {
+    title: string;
+    content: string;
+}>;
+export declare type UpdatePostType = z.infer<typeof updatePostInput>;
---
+++ Frontend/src/components/Quote.tsx
@@ -0,0 +1,16 @@
+export const Quote = () => {
+  return (
+    <div className="bg-slate-100 h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg">
+          <div className="text-3xl font-bold">
+            "The customer service that I received was exceptional. The support
+            team went above and beyond to address my concerns."
+          </div>
+          <div className="text-xl font-semibold pt-3 ">Jules Winnfield</div>
+          <div className="text-m">CEO, Acme Inc</div>
+        </div>
+      </div>
+    </div>
+  );
+};
