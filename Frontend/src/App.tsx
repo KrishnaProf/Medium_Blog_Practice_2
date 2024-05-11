d88e34923a2c60528bfb53897097df5e4e1d0d96
Leela_Krishna <leela_krishna@dell.com>
2024-05-11 19:13:22 +0530

    blogs.tsx updated

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,18 +4,16 @@ import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
 import { Blogs } from "./pages/Blogs";
-import { Landing } from "./pages/Landing";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog" element={<Blog />} />
-          
+
           <Route path="/blogs" element={<Blogs />} />
         </Routes>
       </BrowserRouter>
--- Frontend/src/pages/Blogs.tsx
+++ Frontend/src/pages/Blogs.tsx
@@ -1,33 +1,14 @@
 import { Appbar } from "../components/Appbar";
-import { BlogSkeleton } from "../components/BlogSkeleton";
 import { Blogcard } from "../components/Blogcard";
-import { useBlogs } from "../hooks";
 
 export const Blogs = () => {
-  const { loading, blogs } = useBlogs();
-  if (loading) {
-    return (
-      <div>
-        <Appbar />
-        <div className="flex justify-center">
-          <div>
-            <BlogSkeleton />
-            <BlogSkeleton />
-            <BlogSkeleton />
-            <BlogSkeleton />
-            <BlogSkeleton />
-          </div>
-        </div>
-      </div>
-    );
-  }
   return (
     <div>
       <Appbar />
       <div className="flex justify-center">
         <div>
           <Blogcard
-            Author="Krishna"
+            Author="Krishnan"
             title="New react feature"
             BlogContent="React new feature details"
             PublishedDate="feb 12th 2024"
