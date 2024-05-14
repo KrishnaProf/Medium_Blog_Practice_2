df61dd02a072127262c051ca3a97b00a2b1c3ac3
Leela_Krishna <leela_krishna@dell.com>
2024-05-14 10:25:41 +0530

    Landing page added

--- Frontend/src/pages/Blogs.tsx
+++ Frontend/src/pages/Blogs.tsx
@@ -1,27 +1,40 @@
 import { Appbar } from "../components/Appbar";
+import { BlogSkeleton } from "../components/BlogSkeleton";
 import { Blogcard } from "../components/Blogcard";
+import { useBlogs } from "../hooks";
 
 export const Blogs = () => {
+  const { loading, blogs } = useBlogs();
+  if (loading) {
+    return (
+      <div>
+        <Appbar />
+        <div className="flex justify-center">
+          <div>
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       <Appbar />
       <div className="flex justify-center">
         <div>
-          <Blogcard
-            Author="Krishnan"
-            title="New react feature"
-            BlogContent="React new feature details"
-            PublishedDate="feb 12th 2024"
-            id={21}
-          />
-
-          <Blogcard
-            Author="Vani"
-            title="About React"
-            BlogContent="React is a library for building user interfaces"
-            PublishedDate="feb 18th 2024"
-            id={22}
-          />
+          {blogs.map((blog: any) => (
+            <Blogcard
+              id={blog.id}
+              title={blog.title}
+              BlogContent={blog.BlogContent}
+              Author={blog.author}
+              PublishedDate={blog.publishedDate}
+            />
+          ))}
         </div>
       </div>
     </div>
